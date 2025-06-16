a34a5fe71b1fb049b105e6ab96d0b0b547417928
deventetrus <148906797+deventetrus@users.noreply.github.com>
2025-06-16 13:50:40 -0400

    Create sampleList.js

---
+++ sampleList.js
@@ -0,0 +1,7 @@
+window.videoData = {
+  "videos": {
+    "Angular Basics": "https://www.youtube.com/watch?v=htzY8XqzpDI",
+    "React Tutorial": "https://www.youtube.com/watch?v=4UZrsTqkcW4",
+    "JavaScript for Beginners": "https://www.youtube.com/watch?v=W6NZfCO5SIk"
+  }
+};
