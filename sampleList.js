9a9056edbbfbed5456b105746273df24d10df46d
deventetrus <148906797+deventetrus@users.noreply.github.com>
2025-06-16 15:50:24 -0400

    Update sampleList.js

--- sampleList.js
+++ sampleList.js
@@ -1,6 +1,6 @@
 window.sampleLinksData = {
   "links": {
-    "Computer Basics and IT Support": "https://docs.google.com/presentation/d/e/2PACX-1vTIb80k95r-f6FiqBWMM21SEMqeUMLXdBNoLrN-9htwEyVORwsNAp02G_-0Fj2weOClDHUSSz8E11cO/pubembed?start=true&loop=false&delayms=3000",
+    "Computer Basics and IT Support": "https://docs.google.com/presentation/d/e/2PACX-1vT4Ecb4y5LXoxMs331RIds9WfNF0Ggzj1XIw5tRRLACEXicadvgWnD23WwH7zuLRPeqJtJWRDzwMuK_/pub?start=true&loop=true&delayms=15000",
     "React Tutorial": "https://www.youtube.com/watch?v=4UZrsTqkcW4",
     "JavaScript for Beginners": "https://www.youtube.com/watch?v=W6NZfCO5SIk"
   },
