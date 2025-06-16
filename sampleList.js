adaae4031ceea3e8781f2870102dff831d1291f6
deventetrus <148906797+deventetrus@users.noreply.github.com>
2025-06-16 14:18:23 -0400

    Update sampleList.js

--- sampleList.js
+++ sampleList.js
@@ -3,5 +3,8 @@ window.sampleLinksData = {
     "Computer Basics and IT Support": "https://docs.google.com/presentation/d/e/2PACX-1vTIb80k95r-f6FiqBWMM21SEMqeUMLXdBNoLrN-9htwEyVORwsNAp02G_-0Fj2weOClDHUSSz8E11cO/pubembed?start=true&loop=false&delayms=3000",
     "React Tutorial": "https://www.youtube.com/watch?v=4UZrsTqkcW4",
     "JavaScript for Beginners": "https://www.youtube.com/watch?v=W6NZfCO5SIk"
+  },
+  "activityLinks":{
+    "Module 1: Introduction to Computers": "https://www.youtube.com/watch?v=OE4kP4Fh7Pg"
   }
 };
