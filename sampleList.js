819a924d017844e4a59ccc990eec393e8675239c
deventetrus <148906797+deventetrus@users.noreply.github.com>
2025-06-16 15:18:14 -0400

    Update sampleList.js

--- sampleList.js
+++ sampleList.js
@@ -5,6 +5,12 @@ window.sampleLinksData = {
     "JavaScript for Beginners": "https://www.youtube.com/watch?v=W6NZfCO5SIk"
   },
   "activityLinks":{
-    "Module 1: Introduction to Computers": "https://www.youtube.com/embed/w84t03oQMQQ?si=aQ2HTrGdO2ODRhEF"
+    "Module 1: Introduction to Computers": "https://www.youtube.com/embed/w84t03oQMQQ?si=aQ2HTrGdO2ODRhEF",
+    "Welding Basics":"https://www.youtube.com/embed/ZZvMsnSUDqo?si=ttb-cKqx3I8cjCj7",
+    "Safety Precautions":"https://www.youtube.com/embed/Gs05yeLyOXY?si=ohRUd9wf07yymZlX",
+    "Fundamentals of Welding":"https://www.youtube.com/embed/ZZvMsnSUDqo?si=ttb-cKqx3I8cjCj7",
+    "Welding Beginner Basics":"https://www.youtube.com/embed/n55ERzkl7Po?si=Q8uKu4DGtcOe63D1",
+    "Understanding the Metals":"https://www.youtube.com/embed/PaGJwOPg2kU?si=tK0FP1u-HqITqcUi",
+    "Welding and Joining Technologies":"https://www.youtube.com/embed/nkUJf0tt6Lk?si=7E-FEMqmT2avwASE"
   }
 };
