1668519e072440718f31a4e09517513835c100a1
deventetrus <148906797+deventetrus@users.noreply.github.com>
2025-06-17 01:14:02 -0400

    Update sampleList.js

--- sampleList.js
+++ sampleList.js
@@ -1,6 +1,7 @@
 window.sampleLinksData = {
   "links": {
     "Basic Welding Course": "https://docs.google.com/presentation/d/e/2PACX-1vQc0XVtCDK09U0sJZKHS3YVnVDyr1zXWIyHXWme0-DrVHHWre3rvFV-BU8Qo5tZCQ/pubembed?start=true&loop=false&delayms=10000",
+    "Comprehensive Plubming for Beginners": "https://docs.google.com/presentation/d/e/2PACX-1vRixmmM26UJUyebOUc9H6GCLx1gonGbHVO6ETQyDRMUA_1EpiXTyBiHCP2--YIQfgQmQFrJa0_IXvi1/pubembed?start=true&loop=false&delayms=10000"
   },
   "activityLinks":{
     "Module 1: Introduction to Computers": "https://www.youtube.com/embed/w84t03oQMQQ?si=aQ2HTrGdO2ODRhEF",
@@ -9,6 +10,18 @@ window.sampleLinksData = {
     "Fundamentals of Welding":"https://www.youtube.com/embed/ZZvMsnSUDqo?si=ttb-cKqx3I8cjCj7",
     "Welding Beginner Basics":"https://www.youtube.com/embed/n55ERzkl7Po?si=Q8uKu4DGtcOe63D1",
     "Understanding the Metals":"https://www.youtube.com/embed/PaGJwOPg2kU?si=tK0FP1u-HqITqcUi",
-    "Welding and Joining Technologies":"https://www.youtube.com/embed/nkUJf0tt6Lk?si=7E-FEMqmT2avwASE"
+    "Welding and Joining Technologies":"https://www.youtube.com/embed/nkUJf0tt6Lk?si=7E-FEMqmT2avwASE",
+    "Lesson 1.1: What is Plumbing?":"https://www.youtube.com/embed/SsPtZqOAf4s?si=ZxbBczbCNjXT8SsK",
+    "Lesson 1.2: Types of Plumbing Systems":"https://www.youtube.com/embed/5U4M6mZyXzg?si=snnnurnEF6WQRZZA",
+    "Lesson 2.1: Hand Tools and Their Uses":"https://www.youtube.com/embed/V_SzExu2aV0?si=bKjQyMURWVCvRk7M",
+    "Lesson 2.2: Power Tools in Plumbing":"https://www.youtube.com/embed/RxorPsoJSUo?si=XnwHJgZ40NhZZfy8",
+    "Lesson 2.3: Safety Practices":"https://www.youtube.com/embed/gyWrbmQp5a0?si=U4TRk-tBYKr18Dxi",
+    "Lesson 3: Plumbing Materials and Fittings":"https://www.youtube.com/embed/5U4M6mZyXzg?si=2H2vq5D3KqmJUjRI",
+    "Lesson 4: Reading Plumbing Schematics":"https://www.youtube.com/embed/g8VyP4fFWbg?si=jgwEI0q7b3YiJJyi",
+    "Lesson 5: Water Pressure, Flow Rate, and Sizing":"https://www.youtube.com/embed/EYg_5QGwxgM?si=liiW3Nhtp9h_uzHw",
+    "Lesson 6: Drainage, Waste, and Vent (DWV) Systems":"https://www.youtube.com/embed/D_5iyjXUV-o?si=QcU7-ILcE_O5aw6b",
+    "Lesson 8.1: Leak Detection and Repair":"https://www.youtube.com/embed/tyVuozXM3WA?si=aSmAgTzSEF6CTKCq",
+    "Lesson 8.2: Clearing Blockages":"https://www.youtube.com/embed/NFYFaczoWt0?si=jLV9sROE6HlPO9f4",
+    "Lesson 8.3: Repairing/Replacing Faucets and Valves":"https://www.youtube.com/embed/rBJ0dBae-YM?si=fPI3CZkfaclb6TRg",
   }
 };
